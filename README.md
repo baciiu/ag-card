74f37115fabe8e85a2c19f32e52ae9132a009847
baciiu <60070747+baciiu@users.noreply.github.com>
2022-12-05 11:39:20 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ag-card
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-mat-card-example-4pc81h)
---
+++ src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent  {
+  name = 'Angular 6';
+  longText = `The Shiba Inu is the smallest of the six original and distinct spitz breeds of dog
+  from Japan. A small, agile dog that copes very well with mountainous terrain, the Shiba Inu was
+  originally bred for hunting.`;
+
+  patientName = "Max Musterman";
+  patientGD = "01.01.1980";
+}
